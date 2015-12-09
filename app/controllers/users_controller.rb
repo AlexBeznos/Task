70a5b333cef40d6a4fbeda10af6157bc46d640a5
Alex <beznosa@yahoo.com>
2015-12-09 11:42:29 +0200

    Update users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,13 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
-    respond_to do |format|
-      format.html { render json: user_to_json(@users) }
-    end
+    render json: user_to_json(@users) # #user_to_json is shit find out how to make this by rabl gem or jbuilder gem
   end
 
   def show
     @user = User.find(params[:id])
     respond_to do |format|
-      format.html { render json: user_to_json(@user) }
+      format.html { render json: user_to_json(@user) } # same as in index action
     end
   end
 
