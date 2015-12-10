3583cee14b2e4fba321fb52429db85a3ac1ed847
Alex <beznosa@yahoo.com>
2015-12-10 11:03:16 +0200

    Update place.rb

--- app/models/place.rb
+++ app/models/place.rb
@@ -4,6 +4,6 @@ class Place < ActiveRecord::Base
             :owner,
             format: {
               with: /\A[a-zA-Z]+\z/,
-              message: "only allows letters" },
+              message: "only allows letters" }, # FIXME: why do you think, address not gonna contain any numerical charachters?
             presence: true
 end
