04bc3b9016af61bc51fc8a1b0bd622585d7962c1
YanekDevelop <yan.summer23@gmail.com>
2015-12-10 18:49:37 +0200

    Misceltainous 1

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,9 +1,8 @@
 class Place < ActiveRecord::Base
   validates :name,
-            :address ,
             :owner,
             format: {
               with: /\A[a-zA-Z]+\z/,
-              message: "only allows letters" },
+              message: "only allows letters" }, # FIXME: why do you think, address not gonna contain any numerical charachters?
             presence: true
 end
