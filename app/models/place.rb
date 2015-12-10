43a9ef53a5247a23b2567a377eec871c2df25d30
YanekDevelop <yan.summer23@gmail.com>
2015-12-10 06:24:45 +0200

    Adds data validations to models

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,2 +1,9 @@
 class Place < ActiveRecord::Base
+  validates :name,
+            :address ,
+            :owner,
+            format: {
+              with: /\A[a-zA-Z]+\z/,
+              message: "only allows letters" },
+            presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
-  
+  validates :name,
+            :last_name ,
+            :city,
+            format: {
+              with: /\A[a-zA-Z]+\z/,
+              message: "only allows letters" },
+            presence: true
 end
