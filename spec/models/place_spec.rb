de4e4355b32a95478ccc6ed3e7484ce106772ba0
Alex <beznosa@yahoo.com>
2015-12-11 02:34:29 +0200

    Update place_spec.rb

--- spec/models/place_spec.rb
+++ spec/models/place_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe Place do
     it "is valid with a name, address and owner" do
-    place = Place.new name:        'Vasya',
+    place = Place.new name:        'Vasya',  # it's shit. find out how to use shoulda_matchers(for validations testing) and factory_girl(for factory creations) also in factory creation u need to use Faker gem to make fake data
                      address:     'Bitch 17',
                      owner:       'Big Dick'
 
