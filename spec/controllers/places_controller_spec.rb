fda4d2c1418f67b16e262074bf2abb1db8341df1
Alex <beznosa@yahoo.com>
2015-12-11 02:37:01 +0200

    Create places_controller_spec.rb

---
+++ spec/controllers/places_controller_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+describe PlacesController do
+  describe '#index' do
+  end
+  describe '#show' do
+    # TODO: test all actions in places and users controllers. u need to test both positive and negative cases.
+  end
+end
